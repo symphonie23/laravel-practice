927b90929b343877af65c4e68ceda55d9fa147e4
gparawan <102518646+symphonie23@users.noreply.github.com>
2023-03-08 13:14:57 +0800

    removed an unecessary space

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,3 @@ Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/task', [TaskController::class, 'store']);
 Route::post('/tasks', [TaskController::class, 'store']);
 Route::delete('/task/{task}', [TaskController::class, 'destroy']);
-
